488554a729599f83788a0d08cee9e96e1043ed51
CnomDev2 <dev2@cnomweb.com>
2025-08-07 20:11:17 +0100

    upt

--- auth.ts
+++ auth.ts
@@ -6,7 +6,7 @@ import { CredentialsSignin } from "next-auth"
 import { signInSchema } from "./lib/zod"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  trustHost: process.env.NODE_ENV !== "production",
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
